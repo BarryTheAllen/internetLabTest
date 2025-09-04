a335b810044651237f4f0106963b5961280d117d
BarryTheAllen <radyga936@gmail.com>
2025-09-04 16:01:51 +0400

    Update webpack.config.js

--- InternetLabtest/webpack.config.js
+++ InternetLabtest/webpack.config.js
@@ -4,30 +4,27 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
-  
+  const isGitHubPages = process.env.NODE_ENV === 'production';
+
+  const publicPath = isGitHubPages ? '/internetLabTest/' : '/';
+
   return {
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: isProduction ? '[name].[contenthash].js' : '[name].js',
       clean: true,
+      publicPath: publicPath
     },
     module: {
       rules: [
         {
-          test: /\.css$/i,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader'
-          ],
-        },
-        {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
           type: 'asset/resource',
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: 'asset/resource',
+          generator: {
+            filename: 'images/[name].[hash][ext]',
+            publicPath: `${publicPath}images/`
+          }
         },
       ],
     },
@@ -36,15 +33,6 @@ module.exports = (env, argv) => {
         template: './src/index.html',
         filename: 'index.html',
       }),
-      ...(isProduction ? [new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css',
-      })] : []),
     ],
-    devServer: {
-      static: './dist',
-      hot: true,
-      open: true,
-      port: 3000,
-    },
   };
 };
