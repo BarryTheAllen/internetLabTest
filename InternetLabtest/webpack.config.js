afe47f5e2879ea1a22717ea11e072c7d9603f25d
BarryTheAllen <radyga936@gmail.com>
2025-09-04 17:04:07 +0400

    Update webpack.config.js

--- InternetLabtest/webpack.config.js
+++ InternetLabtest/webpack.config.js
@@ -1,59 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const GITHUB_USERNAME = 'BarryTheAllen';
-
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-
-  return {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
-      clean: true,
-      publicPath: `https://${GITHUB_USERNAME}.github.io/internetLabTest/`
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader'
-          ],
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/internetLabTest/',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
         },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'images/[hash][ext][query]'
-          }
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'fonts/[hash][ext][query]'
-          }
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-      }),
-      ...(isProduction ? [new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css',
-      })] : []),
+      },
     ],
-    devServer: {
-      static: './dist',
-      hot: true,
-      open: true,
-      port: 3000,
-    },
-  };
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
