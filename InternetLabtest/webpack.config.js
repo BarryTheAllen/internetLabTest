863c6a12af2057ba328aaa93bab60e6f6bb30f58
BarryTheAllen <radyga936@gmail.com>
2025-09-04 16:03:14 +0400

    Update webpack.config.js

--- InternetLabtest/webpack.config.js
+++ InternetLabtest/webpack.config.js
@@ -4,9 +4,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
-  const isGitHubPages = process.env.NODE_ENV === 'production';
-
-  const publicPath = isGitHubPages ? '/internetLabTest/' : '/';
 
   return {
     entry: './src/index.js',
@@ -14,16 +11,29 @@ module.exports = (env, argv) => {
       path: path.resolve(__dirname, 'dist'),
       filename: isProduction ? '[name].[contenthash].js' : '[name].js',
       clean: true,
-      publicPath: publicPath
     },
     module: {
       rules: [
         {
+          test: /\.css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'postcss-loader'
+          ],
+        },
+        {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
           type: 'asset/resource',
           generator: {
-            filename: 'images/[name].[hash][ext]',
-            publicPath: `${publicPath}images/`
+            filename: 'images/[hash][ext][query]'
+          }
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[hash][ext][query]'
           }
         },
       ],
@@ -33,6 +43,15 @@ module.exports = (env, argv) => {
         template: './src/index.html',
         filename: 'index.html',
       }),
+      ...(isProduction ? [new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+      })] : []),
     ],
+    devServer: {
+      static: './dist',
+      hot: true,
+      open: true,
+      port: 3000,
+    },
   };
 };
