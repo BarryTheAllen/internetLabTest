80124e9c69ee5d6d8954b27ccf9b270e9f472cb5
BarryTheAllen <radyga936@gmail.com>
2025-09-04 15:48:23 +0400

    Update webpack.config.js

--- InternetLabtest/webpack.config.js
+++ InternetLabtest/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+т
 const GITHUB_USERNAME = 'BarryTheAllen';
 
 module.exports = (env, argv) => {
